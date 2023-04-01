e9aa15703d52ccf5923bae5638ec32251e23ea3e
Martina2911 <tinasamy1122@gmail.com>
2023-04-01 23:04:07 +0200

    [Development] Adds test cases for validity subCode

--- Testing_project_10/src/main/java/Testing/isValidTester.java
+++ Testing_project_10/src/main/java/Testing/isValidTester.java
@@ -1,14 +1,113 @@
 package Testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import com.mycompany.testing_project_10.Verfication;
 
 class isValidTester {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testA1() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA2() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode(null);
+	    assertEquals(expected,actual);
 	}
 
+	@Test
+	void testA3() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("A2345");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA4() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("CS345678");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA5() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("CSE5678");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA6() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("Cse457g");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA7() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("Cse457s");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA8() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("CSE123s");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA9() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("cse765s");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA10() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("cSe128s");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA11() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("cse%28s");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testA12() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectCode("c@e128s");
+	    assertEquals(expected,actual);
+	}
+	
+	
+	
+	
+	
+	
 }
+
