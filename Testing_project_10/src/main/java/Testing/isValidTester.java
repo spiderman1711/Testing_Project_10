22b85f93a0f6bb3c5b1a3084b6fd855dfb4944e4
Martina2911 <tinasamy1122@gmail.com>
2023-04-01 21:59:49 +0200

    [Development] Adds test cases for validity sbjName

--- Testing_project_10/src/main/java/Testing/isValidTester.java
+++ Testing_project_10/src/main/java/Testing/isValidTester.java
@@ -4,11 +4,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class isValidTester {
+import com.mycompany.testing_project_10.Verfication;
+
+class isValidTester 
+{
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test1() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("");
+	    assertEquals(expected,actual);
 	}
 
+	@Test
+	void test2() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName(null);
+	    assertEquals(expected,actual);
+	}
+ 
+	@Test
+	void test3() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("Maths");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void test4() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("maths");
+	    assertEquals(expected,actual);
+	}
+
+	@Test
+	void test5() {
+		boolean expected = true;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("Introduction to Mathematics");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void test6() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("Maths123");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void test7() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("1234");
+	    assertEquals(expected,actual);
+	}
+	
+	@Test
+	void test8() {
+		boolean expected = false;
+		Verfication verfication = new Verfication();
+		boolean actual = verfication.isValidSubjectName("%%##@!*");
+	    assertEquals(expected,actual);
+	}
 }
