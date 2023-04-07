2408ee99a0cbdb6c24bf5987b5bfd3fe39b70ced
Martina2911 <tinasamy1122@gmail.com>
2023-04-07 17:50:33 +0200

    [Development]Adds test cases for validity of Midterm Exam Mark

--- Testing_project_10/src/main/java/Testing/IsValidMidtermExam.java
+++ Testing_project_10/src/main/java/Testing/IsValidMidtermExam.java
@@ -6,34 +6,34 @@ import org.junit.jupiter.api.Test;
 
 import com.mycompany.testing_project_10.Verfication;
 
-class isvalidFinalexamMarkTester {
+class IsValidMidtermExam {
 
 	@Test
 	void testValidInputs() {
     boolean expected=true;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(60,100);
+    boolean actual=verfication.isValidMidtermExamsMark(20,100);
 	}
 	
 	@Test
 	void testNegValue() {
     boolean expected=false;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(-60,100);
+    boolean actual=verfication.isValidMidtermExamsMark(-20,100);
 	}
 	
 	@Test
 	void testOutRangeInput() {
     boolean expected=false;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(30,100);
+    boolean actual=verfication.isValidMidtermExamsMark(30,100);
 	}
 	
 	@Test
 	void testFulMarkequalsInput() {
     boolean expected=true;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(100,100);
+    boolean actual=verfication.isValidMidtermExamsMark(100,100);
 	}
 	
 
@@ -41,28 +41,28 @@ class isvalidFinalexamMarkTester {
 	void testZeroInput() {
     boolean expected=false;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(0,100);
+    boolean actual=verfication.isValidMidtermExamsMark(0,100);
 	}
 	
 	@Test
 	void testInputMoreThanFullMark() {
     boolean expected=false;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(1000,100);
+    boolean actual=verfication.isValidMidtermExamsMark(1000,100);
 	}
 	
 	@Test
 	void testInputDoubleInputs() {
     boolean expected=true;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(70.6,100);
+    boolean actual=verfication.isValidMidtermExamsMark(16.5,100);
 	}
 	
 	@Test
 	void testInputNegDoubleInputs() {
     boolean expected=false;
     Verfication verfication = new Verfication(); 
-    boolean actual=verfication.isValidFinalExamMark(-70.6,100);
+    boolean actual=verfication.isValidMidtermExamsMark(-16.5,100);
 	}
 
 }
